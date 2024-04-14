712f49155e70aa2da4e15298fb743f7ed82f535b
Yousung Kim <yousungkim98@gmail.com>
2024-04-13 21:50:33 -0400

    frontened

--- app/clien-react/src/App.js
+++ app/clien-react/src/App.js
@@ -6,20 +6,22 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import { useState } from "react";
 import StatusBox from "./components/StatusBox";
+import html2canvas from "html2canvas";
 // import Zoom from "./components/Zoom";
 
 function App() {
   // variable for user status
   const [userStatus, setUserStatus] = useState("good");
+  const [meeting, setMeeting] = useState("83243247640");
 
   const client = ZoomMtgEmbedded.createClient();
   var authEndpoint = "http://localhost:4000";
   var sdkKey = "4VvHTcUiRoqpvg0rsBe5kw";
-  var meetingNumber = "85719114952";
-  var passWord = "m5e1q2";
+  var meetingNumber = "83243247640";
+  var passWord = "9u0D8c";
   var role = 1;
-  var userName = ""
-  var userEmail = "";
+  var userName = "allykim@seas.upenn.edu";
+  var userEmail = "allykim@seas.upenn.edu";
   var registrantToken = "";
   var zakToken = "";
 
@@ -43,6 +45,39 @@ function App() {
       });
   }
 
+  const captureScreenshot = () => {
+    // Capture the screenshot from the entire window/document
+    html2canvas(document.body)
+      .then((canvas) => {
+        // Convert canvas to Blob
+        canvas.toBlob((blob) => {
+          // Check if blob is null
+          if (!blob) {
+            console.error("Blob was null");
+            return;
+          }
+          const formData = new FormData();
+          formData.append("screenshot", blob, "screenshot.png");
+
+          // Send the Blob to the Flask server
+          fetch("http://localhost:5000/upload", {
+            method: "POST",
+            body: formData,
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log("Success:", data);
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        }, "image/png");
+      })
+      .catch((err) => {
+        console.error("Error capturing screenshot:", err);
+      });
+  };
+
   function startMeeting(signature) {
     let meetingSDKElement = document.getElementById("meetingSDKElement");
 
@@ -50,6 +85,21 @@ function App() {
       .init({
         zoomAppRoot: meetingSDKElement,
         language: "en-US",
+        customize: {
+          video: {
+            // isResizable: true,
+            viewSizes: {
+              default: {
+                width: 720,
+                height: 411,
+              },
+              ribbon: {
+                width: 300,
+                height: 700,
+              },
+            },
+          },
+        },
         patchJsMedia: true,
       })
       .then(() => {
@@ -82,18 +132,26 @@ function App() {
       <main>
         {/* For Component View */}
         <div className="flex flex-row">
-          <div className="flex w-1/2 p-60">ZOOM BOX</div>
-          <div className="flex flex-col w-1/2 p-60 border">
+          <div className="flex w-1/2">
+            <div id="meetingSDKElement" className="flex p-10">
+              {/* Zoom Meeting SDK Component View Rendered Here */}
+            </div>
+          </div>
+          <div className="flex flex-col w-1/2">
             <div className="flex h-3/5">ChatBox</div>
             <StatusBox userStatus={userStatus} />
           </div>
         </div>
 
-        <div id="meetingSDKElement">
-          {/* Zoom Meeting SDK Component View Rendered Here */}
-        </div>
+        {/* <input
+          type="text"
+          value="Set Meeting Number"
+          onClick={() => setMeeting(meetingNumber)}
+        /> */}
 
         <button onClick={getSignature}>Join Meeting</button>
+        <button onClick={captureScreenshot}>Screenshot Button</button>
+        {/* <ScreenshotButton /> */}
       </main>
       <Footer />
     </div>
