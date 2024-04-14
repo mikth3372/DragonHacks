c86fbe9f94556a1eecbcd8e9e7e250bb293178e9
mikth3372 <mikth@seas.upenn.edu>
2024-04-13 22:40:34 -0400

    adding cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+CORS(app)
 
 # Import other parts of your application
 from app import routes
